55f5aac6aafd51f8c0107089ef306dd0506c44a8
That Guy <ThatGuyTF3@outlook.com>
2024-12-28 01:18:49 -0800

    $(date '+%D %r')

--- configuration.nix
+++ configuration.nix
@@ -149,7 +149,17 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = false;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = null;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
