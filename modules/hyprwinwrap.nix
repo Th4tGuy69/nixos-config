2bd75ed94c7493df07aba0519a9d0ed0d6d57a05
That Guy <ThatGuyTF3@outlook.com>
2025-08-27 12:42:05 -0700

    08/27/25 12:42:05 PM

--- modules/hyprwinwrap.nix
+++ modules/hyprwinwrap.nix
@@ -3,8 +3,7 @@
 let
   clock-bg = pkgs.writeShellScriptBin "clock-bg" ''
     #!${pkgs.bash}/bin/bash  
-    export TZ=America/Los_Angeles
-    clock-rs -bst --fmt "%A, %B %d, %Y"
+    clock-rs -bt --fmt "%A, %B %d, %Y"
   '';  
 in
 
@@ -12,7 +11,7 @@ in
   home.packages = [ pkgs.clock-rs pkgs.kitty ];
 
   wayland.windowManager.hyprland.settings.exec-once = [
-    "${pkgs.kitty}/bin/kitty --class 'clock-bg' ${clock-bg}/bin/clock-bg"
+    "TZ=America/Los_Angeles ${pkgs.kitty}/bin/kitty --class 'clock-bg' ${clock-bg}/bin/clock-bg"
   ];
 
   wayland.windowManager.hyprland.settings.plugin.hyprwinwrap = {
