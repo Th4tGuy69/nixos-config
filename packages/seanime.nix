f5ddee4429530af3ecfacfa1fe1007a03fad82a8
That Guy <ThatGuyTF3@outlook.com>
2025-08-31 11:50:50 -0700

    08/31/25 11:50:50 AM

--- packages/seanime.nix
+++ packages/seanime.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "seanime";
-  version = "2.9.7";
+  version = "2.9.9";
 
   src = pkgs.fetchurl {
     url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-${version}_Linux_x86_64.tar.gz";
-    hash = "sha256-nkRU6u7dgQPUKjlD6618aaIXYZy3LNLimmCieBDlFHg=";
+    hash = "sha256-KfaJgLjQxV7+kDcGvZjqhz/nvPg4R+y9Mlknm5H1nCo=";
   };
 
   phases = [ "installPhase" ];
