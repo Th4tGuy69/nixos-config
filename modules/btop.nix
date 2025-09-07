3fee5a7b9c92e2b64083d8e86cb7145f04fbb925
That Guy <ThatGuyTF3@outlook.com>
2025-09-07 10:12:09 -0700

    09/07/25 10:12:09 AM

--- modules/btop.nix
+++ modules/btop.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.btop = {
     enable = true;
+    package = pkgs.btop-rocm;
     settings = {
       shown_boxes = "cpu mem net gpu0";
       base_10_sizes = true;
