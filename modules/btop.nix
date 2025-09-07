822294253b84fc0d3ff4fa87e844a13d708bd719
That Guy <ThatGuyTF3@outlook.com>
2025-09-07 10:18:37 -0700

    09/07/25 10:18:37 AM

--- modules/btop.nix
+++ modules/btop.nix
@@ -5,8 +5,10 @@
     enable = true;
     package = pkgs.btop-rocm;
     settings = {
+      update_ms = 1300;
       shown_boxes = "cpu mem net gpu0";
       base_10_sizes = true;
+      disks_filter = "exclude=/boot /windows";
     };
   };
 }
