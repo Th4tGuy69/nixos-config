3cd48db9b7332b9b2a27150b5ccfcd7742081269
That Guy <ThatGuyTF3@outlook.com>
2025-10-26 11:10:58 -0700

    10/26/25 11:10:58 AM

--- modules/sops.nix
+++ modules/sops.nix
@@ -6,20 +6,9 @@
   home.packages = with pkgs; [ sops ];
   
   sops = {
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age-key.txt"; # must have no password!
-    # It's also possible to use a ssh key, but only when it has no password:
-    #age.sshKeyPaths = [ "/home/user/path-to-ssh-key" ];
-    # defaultSopsFile = "${config.home.homeDirectory}/.config/sops/secrets.yaml";
-    defaultSopsFile = ../secrets/secrets.yaml; # Must be absolute
-    # defaultSopsFile = "/home/thatguy/.config/sops/secrets.yaml";
-    # defaultSopsFile = builtins.toString ../secrets/secrets.yaml;
-    
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age-key.txt";
+    defaultSopsFile = ../secrets/secrets.yaml;
     secrets.test = {
-      # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
-
-      # %r gets replaced with a runtime directory, use %% to specify a '%'
-      # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
-      # DARWIN_USER_TEMP_DIR) on darwin.
       path = "%r/test.txt";
     }; 
   };
