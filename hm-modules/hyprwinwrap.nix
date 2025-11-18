893f68f94c06640b276a4de388921b66a7eb0865
That Guy <ThatGuyTF3@outlook.com>
2025-11-17 23:19:15 -0800

    11/17/25 11:19:15 PM

--- hm-modules/hyprwinwrap.nix
+++ hm-modules/hyprwinwrap.nix
@@ -1,28 +1,39 @@
-{ pkgs, inputs, system, ... }:
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}:
 
 let
   clock-bg = pkgs.writeShellScriptBin "clock-bg" ''
     #!${pkgs.bash}/bin/bash
+    ${pkgs.coreutils}/bin/sleep 1 && ${pkgs.cava}/bin/cava
     # clock-rs -bst --fmt "%A, %B %d, %Y"
-    /home/thatguy/Documents/Projects/rinow/target/debug/rinow
-  '';  
+    # /home/thatguy/Documents/Projects/rinow/target/debug/rinow
+  '';
 in
 
 {
-  home.packages = [ pkgs.clock-rs pkgs.bash pkgs.kitty ];
+  home.packages = [
+    pkgs.clock-rs
+    pkgs.bash
+    pkgs.kitty
+  ];
 
   wayland.windowManager.hyprland = {
     plugins = with inputs.hyprland-plugins.packages.${system}; [
-      hyprwinwrap  
+      hyprwinwrap
     ];
 
     settings = {
       exec-once = [
         "${pkgs.kitty}/bin/kitty --class 'clock-bg' ${clock-bg}/bin/clock-bg"
       ];
-      
+
       plugin.hyprwinwrap = {
-        class = "clock-bg";   
+        class = "clock-bg";
+        title = "clock-bg";
       };
     };
   };
