d1a804b37dfc4114cad156e54ab2a228c6410cdd
That Guy <ThatGuyTF3@outlook.com>
2025-12-12 07:52:21 -0800

    12/12/25 07:52:21 AM

--- flakes/zen-browser/flake.nix
+++ flakes/zen-browser/flake.nix
@@ -14,11 +14,11 @@
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems f;
 
-      version = "1.17.12b";
+      version = "1.17.13b";
       downloadUrl = {
         "x86_64-linux" = {
           url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-          sha256 = "sha256:18a3xy3g6nxbm1pvsznjk68c0zvbzs6pka3y919ssvi2fb7hw2vi";
+          sha256 = "sha256:1asfh4crjpnmz3g2kaiipb7hzlpdmk8af9srn8ragv2q12f4qqkg";
         };
         "aarch64-linux" = {
           url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-aarch64.tar.xz";
