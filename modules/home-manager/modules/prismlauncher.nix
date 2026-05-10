a8c07f79f532e6b14ffd47652691db99bfc52d26
That Guy <ThatGuyTF3@outlook.com>
2026-05-10 01:14:03 -0700

    05/10/26 01:14:03 AM

--- modules/home-manager/modules/prismlauncher.nix
+++ modules/home-manager/modules/prismlauncher.nix
@@ -4,18 +4,16 @@
   flake.homeModules.prismlauncher =
     { pkgs, ... }:
     {
-      # libwayland-client error fix
-      home.sessionVariables = {
-        LD_LIBRARY_PATH =
-          with pkgs;
-          lib.makeLibraryPath [
-            wayland
-          ];
+      home.file.".local/share/PrismLauncher/themes/Amoled" = {
+        source = pkgs.fetchzip {
+          url = "https://github.com/PrismLauncher/Themes/releases/latest/download/Amoled-theme.zip";
+          sha256 = "sha256-rjGTYC7nq9fTT1yVQPaD+aRBCHXnhjfG260PNNpeTrU=";
+        };
       };
 
       programs.prismlauncher = {
         enable = true;
-        extraPackages = [ ];
+
         package =
           with pkgs;
           (prismlauncher.override {
@@ -26,7 +24,80 @@
               zulu
             ];
           });
-        settings = { };
+
+        settings = {
+          ApplicationTheme = "Amoled";
+          AutoCloseConsole = false;
+          AutomaticJavaDownload = false;
+          AutomaticJavaSwitch = true;
+          BackgroundCat = "rory-flat";
+          CatFit = "fit";
+          CatOpacity = 100;
+          CentralModsDir = "mods";
+          CloseAfterLaunch = false;
+          ConfigVersion = "1.3";
+          ConsoleFont = "FiraCode Nerd Font Mono";
+          ConsoleFontSize = 11;
+          ConsoleMaxLines = 100000;
+          ConsoleOverflowStop = true;
+          DownloadsDirWatchRecursive = false;
+          EnableFeralGamemode = true;
+          EnableMangoHud = false;
+          FallbackMRBlockedMods = 2;
+          FlameKeyOverride = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm";
+          IconTheme = "flat_white";
+          IconsDir = "icons";
+          IgnoreJavaCompatibility = false;
+          IgnoreJavaWizard = false;
+          InstRenamingMode = "AskEverytime";
+          InstSortMode = "Name";
+          InstanceDir = "instances";
+          JavaArchitecture = 64;
+          JavaDir = "java";
+          JavaRealArchitecture = "amd64";
+          JavaVendor = "Azul Systems, Inc.";
+          Language = "en_US";
+          LaunchMaximized = false;
+          LowMemWarning = true;
+          MaxMemAlloc = 12288;
+          MenuBarInsteadOfToolBar = false;
+          MinMemAlloc = 512;
+          MinecraftWinHeight = 480;
+          MinecraftWinWidth = 854;
+          ModDependenciesDisabled = false;
+          ModMetadataDisabled = false;
+          ModDownloadGeometry = false;
+          MoveModsFromDownloadsDir = false;
+          NumberOfConcurrentDownloads = 6;
+          NumberOfConcurrentTasks = 10;
+          NumberOfManualRetries = 1;
+          OnlineFixes = false;
+          PastebinType = 3;
+          PermGen = 128;
+          ProxyAddr = "127.0.0.1";
+          ProxyPort = 8080;
+          ProxyType = "Default";
+          QuitAfterGameStop = false;
+          RecordGameTime = true;
+          RequestTimeout = 60;
+          SelectedInstance = "1.21.1";
+          ShowConsole = false;
+          ShowConsoleOnError = true;
+          ShowGameTime = true;
+          ShowGameTimeWithoutDays = false;
+          ShowGlobalGameTime = true;
+          ShowModIncompat = true;
+          SkinsDir = "skins";
+          SkipModpackUpdatePrompt = false;
+          StatusBarVisible = true;
+          TheCat = false;
+          ToolbarsLocked = false;
+          UseDiscreteGpu = false;
+          UseNativeGLFW = true;
+          UseNativeOpenAL = false;
+          UseZink = false;
+          UserAskedAboutAutomaticJavaDownload = true;
+        };
       };
     };
 }
