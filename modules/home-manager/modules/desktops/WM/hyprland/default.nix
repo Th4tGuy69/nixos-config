e0e560081da23d8925714b4fc1989b3142675d0e
That Guy <ThatGuyTF3@outlook.com>
2026-05-14 13:35:56 -0700

    05/14/26 01:35:56 PM

--- modules/home-manager/modules/desktops/WM/hyprland/default.nix
+++ modules/home-manager/modules/desktops/WM/hyprland/default.nix
@@ -124,31 +124,6 @@
             no_hardware_cursors = 1;
           };
 
-          animations = {
-            enabled = "yes, please :)";
-
-            bezier = [
-              "easeOutExpo, 0.16, 1, 0.3, 1"
-              "easeOutQuint, 0.23, 1, 0.32, 1"
-              "easeInOutCubic, 0.65, 0.05, 0.36, 1"
-              "linear, 0, 0, 1, 1"
-              "almostLinear, 0.5, 0.5, 0.75, 1.0"
-              "quick, 0.15, 0, 0.1, 1"
-            ];
-
-            animation = [
-              "global, 1, 10, default"
-              "border, 1, 5.39, easeOutQuint"
-              "windows, 1, 4.79, easeOutQuint"
-              "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-              "windowsOut, 1, 1.49, linear, popin 87%"
-              "fadeIn, 1, 1.73, almostLinear"
-              "fadeOut, 1, 1.46, almostLinear"
-              "layers, 1, 3.81, easeOutQuint"
-              "workspaces, 1, 1.94, almostLinear, fade"
-            ];
-          };
-
           input = {
             kb_layout = "us";
             repeat_rate = 35;
