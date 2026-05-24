1fbbbf42aa88bbe85e4c11a27a32891feea71a42
That Guy <ThatGuyTF3@outlook.com>
2026-05-23 23:14:11 -0700

    05/23/26 11:14:11 PM

---
+++ modules/home-manager/modules/wlsunset.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  flake.homeModules.wlsunset =
+    { ... }:
+    {
+      services.wlsunset = {
+        enable = true;
+        latitude = 44.56;
+        longitude = -123.27;
+        temperature = {
+          day = 4000;
+          night = 1600;
+        };
+      };
+    };
+}
