7b963a9d81b6072ddcd1113de690179f01644668
That Guy <ThatGuyTF3@outlook.com>
2026-08-10 10:35:04 -0700

    08/10/26 10:35:04 AM

--- modules/system/services/ollama.nix
+++ modules/system/services/ollama.nix
@@ -7,8 +7,8 @@
       services.ollama = {
         enable = true;
         package = pkgs.ollama-rocm;
-        # home = "/data/Ollama";
-        # loadModels = [ "qwen2.5-coder:14b" ];
+        home = "/data/Ollama";
+        loadModels = [ "qwen2.5-coder:14b" ];
         rocmOverrideGfx = "11.0.0";
       };
     };
